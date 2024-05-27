d4a139a93ce7197f8d11c500effe4114b69b6ce3
AurelSondakh <aurelsondakh@gmail.com>
2024-05-28 01:17:00 +0800

    feat: Setup Navigation Stack

---
+++ App/Router/AppNavigator.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Containers
+import HomePage from '../Containers/HomePage';
+import AddContactPage from '../Containers/AddContactPage';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomePage">
+        <Stack.Screen name="HomePage" component={HomePage} options={{ headerShown: false }} />
+        <Stack.Screen name="Details" component={AddContactPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
